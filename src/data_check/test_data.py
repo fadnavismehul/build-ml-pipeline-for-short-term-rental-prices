82337e2852f0649a6ce8c6327f2745b69b1d5b8b
Mehul Fadnavis <fadnavismehul@gmail.com>
2022-08-06 01:34:56 +0800

    Finished initial project needs

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data):
+    '''
+    Checking if the data is of a reasonable size
+    '''
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data,max_price,min_price):
+    '''
+    Checking if data is within ranges that have been decided
+    '''
+    assert data['price'].max() <= max_price
+    assert data['price'].min() >= min_price
